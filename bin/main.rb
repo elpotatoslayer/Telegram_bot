86ea9355bb6a778a81673014d2fbdf08330c3cb2
Ralph <ralphoburu@gmail.com>
2021-01-27 22:49:27 +0300

    add random method add api for jokes.

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,5 @@
 
 require_relative '../lib/bot.rb'
 require_relative '../lib/jokes.rb'
+
+Bot.new
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,7 +1,7 @@
 require 'telegram_bot'
 require_relative '../config.rb'
-require_relative 'jokes.rb'
-
+require_relative '../lib/jokes.rb'
+require_relative '../lib/random.rb'
 
 class Bot
   def initialize
@@ -14,15 +14,17 @@ class Bot
         case command
         when /greet/i
           reply.text = "Hello, #{message.from.first_name}!"
-        when '/motivate'
-          values = Jokes.new
-          value = values.random_joke
-          bot.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+        when '/joke'
+          joke = Jokes.new
+          jokes = joke.jokes
+          jokes_s = jokes[rand(jokes.size)]
+    
+          bot.send_message(chat_id: message.chat.id, text: " #{jokes[:value]}".to_s, date: message.date)
         else
           reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
         end
         puts "sending #{reply.text.inspect} to @#{message.from.username}"
-        # reply.send_with(bot)
+        reply.send_with(bot)
       end
     end
   end
