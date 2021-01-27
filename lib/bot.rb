8f1016c26e5039c8f2d32b6c28b192add948b319
Ralph <ralphoburu@gmail.com>
2021-01-27 21:05:06 +0300

    update bot to a class

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,19 +1,30 @@
 require 'telegram_bot'
 require_relative '../config.rb'
+require_relative 'jokes.rb'
 
-bot = TelegramBot.new(TELEGRAM_TOKEN_ID)
 
-bot.get_updates do |message|
-  command = message.get_command_for(bot)
-  
-  message.reply do |reply|
-    case command
-    when /greet/i
-      reply.text = "Hello, #{message.from.first_name}!"
-    else
-      reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
+class Bot
+  def initialize
+    bot = TelegramBot.new(TELEGRAM_TOKEN_ID)
+
+    bot.get_updates do |message|
+      command = message.get_command_for(bot)
+      
+      message.reply do |reply|
+        case command
+        when /greet/i
+          reply.text = "Hello, #{message.from.first_name}!"
+        when '/motivate'
+          values = Jokes.new
+          value = values.random_joke
+          bot.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+        else
+          reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
+        end
+        puts "sending #{reply.text.inspect} to @#{message.from.username}"
+        # reply.send_with(bot)
+      end
     end
-    puts "sending #{reply.text.inspect} to @#{message.from.username}"
-    reply.send_with(bot)
   end
 end
+
